806d853d1817f24422719210c9b5e6f041abeec2
immuneQQ <vitalyasatka@gmail.com>
2022-12-13 06:56:08 +0500

    setting ci

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,21 +1,21 @@
-from gendiff import generate_diff
+from gendiff import gendiff
 
 
 def test_gendiff():
     correct = open('tests/fixtures/result1.txt', 'r').read()
-    assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == correct
+    assert gendiff.generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == correct
 
 
 def test_gendiff_empty():
     correct = open('tests/fixtures/result_empty.txt', 'r').read()
-    assert generate_diff('tests/fixtures/empty.json', 'tests/fixtures/empty.json') == correct
+    assert gendiff.generate_diff('tests/fixtures/empty.json', 'tests/fixtures/empty.json') == correct
 
 
 def test_gendiff_one_empty():
     correct = open('tests/fixtures/result_one_empty.txt', 'r').read()
-    assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/empty.json') == correct
+    assert gendiff.generate_diff('tests/fixtures/file1.json', 'tests/fixtures/empty.json') == correct
 
 
 def test_gendiff_similar():
     correct = open('tests/fixtures/result_similar.txt', 'r').read()
-    assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file1.json') == correct
+    assert gendiff.generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file1.json') == correct
